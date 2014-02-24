756a35bd202ae68d1e1461440139653a85682283
Joey Grasso <me@joeygrasso.com>
2014-02-23 20:56:06 -0800

    Test CI form

--- application/views/signin_view.php
+++ application/views/signin_view.php
@@ -1,4 +1,4 @@
-
+<!--
 	  <form class="form-signin" role="form">
         <h2 class="form-signin-heading">Please Sign In</h2>
         <input type="email" class="form-control" placeholder="User Name" required autofocus>
@@ -8,3 +8,26 @@
         </label>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
+-->
+      <?php
+      
+		$attributes = array('class' => 'form=signin', 'role' => 'form');
+		echo form_open(site/login, $attributes)
+			
+			$data = array('name'=>'username','id'=> 'username','value'=> 'User Name','class'=>'form-control');
+			echo form_input($data);
+			
+			$data = array('name'=>'username','id'=> 'username','value'=> 'johndoe','class'=>'form-control','type'=>'password');
+			echo form_input($data);
+			
+			$data = array('name'=> 'remember','id'=> 'remember','value'=> 'remember-me','style'=>'float:none');
+			echo form_radio($data);
+			
+			$data = array('class'=>'btn btn-lg btn-primary btn-block','value'=>'Sign In','type'=>'submit','content'=>'Sign In');
+			echo form_button($data);
+		
+		echo form_close();
+      
+      
+      
+      ?>
