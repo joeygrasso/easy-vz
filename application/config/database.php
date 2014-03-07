ebcdc06b56a2852f0d275f57dcca4219a928c0ed
Joey Grasso <me@joeygrasso.com>
2014-03-07 18:52:08 -0500

    Minor DB changes

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'aquarius';
-$db['default']['database'] = 'virtualization';
+$db['default']['username'] = 'DBuser';
+$db['default']['password'] = 'DBpass';
+$db['default']['database'] = 'DBname';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
